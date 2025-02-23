d29e63bd971d58f6aa6c5f9d2d2d58a425d3cbd1
NanaAchiaa28 <agyemangnanaachiaa03@gmail.com>
2025-02-23 15:05:07 +0000

    Update Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -7,3 +7,18 @@ function Footer() {
 }
 
 export default Footer;
+import React from "react";
+
+const Footer = () => {
+  const footerStyle = {
+    backgroundColor: "#222",
+    color: "white",
+    textAlign: "center",
+    padding: "10px",
+    marginTop: "20px",
+  };
+
+  return <footer style={footerStyle}>© 2025 All Rights Reserved</footer>;
+};
+
+export default Footer;
