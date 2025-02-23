d1cee59d2a8894c952259c65acff918c148b5f86
NanaAchiaa28 <agyemangnanaachiaa03@gmail.com>
2025-02-23 15:17:42 +0000

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -52,3 +52,17 @@ function App() {
 
 export default App;  /
 
+import React from "react";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>React Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
