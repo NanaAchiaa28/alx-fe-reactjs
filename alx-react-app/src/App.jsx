41c32ea67a34869185354c6a5f04fd3bc75ab3ab
NanaAchiaa28 <agyemangnanaachiaa03@gmail.com>
2025-02-23 15:08:14 +0000

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -38,3 +38,23 @@ function App() {
 }
 
 export default App;
+
+import React from "react";
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <UserProfile />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
