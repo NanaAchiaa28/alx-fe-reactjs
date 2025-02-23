4601f3659b9826d709bef1f129cc7c5ce68f0818
NanaAchiaa28 <agyemangnanaachiaa03@gmail.com>
2025-02-23 15:05:36 +0000

    Update Header.jsx

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -7,3 +7,18 @@ function Header() {
 }
 
 export default Header;
+import React from "react";
+
+const Header = () => {
+  const headerStyle = {
+    backgroundColor: "#282c34",
+    color: "white",
+    padding: "20px",
+    textAlign: "center",
+    fontSize: "24px",
+  };
+
+  return <header style={headerStyle}>Welcome to My React App</header>;
+};
+
+export default Header;
