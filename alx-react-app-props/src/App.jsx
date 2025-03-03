2b0d429b8ec122f0d7325156ddc88db05e396004
Nana Achiaa Agyemang <agyemangnanaachiaa03@gmail.com>
2025-03-03 17:14:26 +0000

    Refactored prop drilling to use Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import MainContent from "./components/MainContent";
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <div>
+      <h2>Welcome to the Recipe Sharing App!</h2>
+      <p>Explore and share amazing recipes with others.</p>
+    </div>
+  );
+}
+
+export default MainContent;
