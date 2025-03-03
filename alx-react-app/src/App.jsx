049d04e71b8651b069a620880020b4a075d08622
Nana Achiaa Agyemang <agyemangnanaachiaa03@gmail.com>
2025-03-03 14:28:19 +0000

    Added Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,18 @@
+import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 function App() {
-  return (
-      <div>
-          <WelcomeMessage />
-      </div>
-  );
+    return (
+        <div>
+            <WelcomeMessage />
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
