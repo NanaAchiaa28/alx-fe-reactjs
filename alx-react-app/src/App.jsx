f3bf9a8806e1fbae7dbd1310988e8c5428a65174
Nana Achiaa Agyemang <agyemangnanaachiaa03@gmail.com>
2025-03-03 14:36:40 +0000

    Fixed App.jsx and added UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import React from "react";
+import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <div>
-            <WelcomeMessage />
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
