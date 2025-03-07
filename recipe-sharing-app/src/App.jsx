eb66cda2d9d0506f597a946b37187924bed777b4
Nana Achiaa Agyemang <agyemangnanaachiaa03@gmail.com>
2025-03-07 19:42:16 +0000

    Added search and filtering functionality

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,23 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
-import RecipeDetail from './components/RecipeDetail';
-import EditRecipeForm from './components/EditRecipeForm';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
+import DeleteRecipeButton from "./components/DeleteRecipeButton";
+import SearchBar from "./components/SearchBar"; // Import SearchBar
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Home page listing all recipes */}
-        <Route path="/" element={<RecipeList />} />
-
-        {/* Route to view recipe details, using the recipe ID */}
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-
-        {/* Route to edit an existing recipe */}
-        <Route path="/recipe/:id/edit" element={<EditRecipeForm />} />
-      </Routes>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar /> {/* Add the SearchBar here */}
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/edit/:id" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
+
---
+++ recipe-sharing-app/src/recipeStore.js
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  
+  setSearchTerm: (term) => set((state) => {
+    const filtered = state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(term.toLowerCase())
+    );
+    return { searchTerm: term, filteredRecipes: filtered };
+  }),
+
+  addRecipe: (recipe) => set((state) => ({
+    recipes: [...state.recipes, recipe],
+    filteredRecipes: [...state.recipes, recipe].filter(r =>
+      r.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    ),
+  })),
+}));
+
+export default useRecipeStore;
