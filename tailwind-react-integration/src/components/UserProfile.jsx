1930a0f666d8cf28e4bf66577879bc642b38a2fe
Nana Achiaa Agyemang <agyemangnanaachiaa03@gmail.com>
2025-03-16 20:02:25 +0000

    Fixed responsive container, typography, and image sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-36 sm:h-36 md:w-48 md:h-48 mx-auto"
       />
       <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">
         John Doe
